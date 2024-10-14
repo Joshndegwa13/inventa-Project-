75a8b38e04b4c064bbe1c70964f2de8548caa866
terenceouma <doros.devs@gmail.com>
2024-10-14 12:30:43 +0300

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXqGoiLbOALNnGjFUv64-Z2BfJDiWGdRE",
+  authDomain: "inventa-c085d.firebaseapp.com",
+  projectId: "inventa-c085d",
+  storageBucket: "inventa-c085d.appspot.com",
+  messagingSenderId: "392189773777",
+  appId: "1:392189773777:web:dddf4cda908d72efd082f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
