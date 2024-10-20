bec695ebd48d34f8d647888af9da8079282418b1
terenceouma <doros.devs@gmail.com>
2024-10-20 21:52:21 +0300

    working salespage

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,22 @@ import HomePage from './Components/HomePage';
 import AboutUs from './Components/AboutUs';
 import Contact from './Components/Contact';
 import Reports from './Components/Reports';
+import SalesPage from './Components/SalesPage';
+import PaymentPage from './Components/PaymentPage';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path='reports' element={<Reports />} />
-            <Route path="/aboutus" element={<AboutUs />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="reports" element={<Reports />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/pricing" element={<PaymentPage />} />
+      </Routes>
     );
 };
 
