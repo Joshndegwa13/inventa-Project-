8421f3fe97d7dbe24ee75665c316565b8479c662
Abdishakur-Sharif <shakuritoshariff@gmail.com>
2024-10-20 15:08:55 +0300

    finished auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import ForgotPassword from './components/Forgotpassword';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Authprovider } from './context/authcontext.jsx'
+import { BrowserRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Authprovider>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
+  </Authprovider>
   </StrictMode>,
 )
