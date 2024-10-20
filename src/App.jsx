d8b92ecccfe2ea038a8b68810d15cf326207369f
Abdishakur-Sharif <shakuritoshariff@gmail.com>
2024-10-20 17:22:27 +0300

    merged with home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import ForgotPassword from './components/Forgotpassword';
-
+import Login from './Components/Login';
+import Register from './Components/Register';
+import ForgotPassword from './Components/Forgotpassword';
+import HomePage from './Components/HomePage';
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
+    </Routes>
     </>
   )
 }
