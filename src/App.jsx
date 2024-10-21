63023629869543aa6a685effc98042352d071a12
Stannyut <stanley.njuguna@student.moringaschool.com>
2024-10-22 01:46:59 +0300

    product section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Components/Login';
-import Register from './Components/Register';
-import ForgotPassword from './Components/Forgotpassword';
-import HomePage from './Components/HomePage';
-import AboutUs from './Components/AboutUs';
-import Contact from './Components/Contact';
-import Reports from './Components/Reports';
-import SalesPage from './Components/SalesPage';
-import PaymentPage from './Components/PaymentPage';
-import Products from "./Components/Products";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import ForgotPassword from "./Components/Forgotpassword";
+import HomePage from "./Components/HomePage";
+import AboutUs from "./Components/AboutUs";
+import Contact from "./Components/Contact";
+import Reports from "./Components/Reports";
+import SalesPage from "./Components/SalesPage";
+import PaymentPage from "./Components/PaymentPage";
+import ProductList from "./Components/ProductList";
+import AddProductForm from "./Components/AddProductForm";
 
-const App = () => {
+import React, { useState } from "react";
+import CSVUploadForm from "./Components/ProductList";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
   return (
     <Routes>
-      <Route path="/products" element={<Products />} />
+      <Route
+        path="/add-product"
+        element={
+          <>
+            <AddProductForm
+              onAdd={(product) => setProducts([...products, product])}
+            />
+            <ProductList products={products} setProducts={setProducts} />
+          </>
+        }
+      />
+      <Route
+        path="/upload-csv"
+        element={<CSVUploadForm onProductsUpdated={setProducts} />}
+      />
+      <Route path="/sales" element={<SalesPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+
       <Route path="/" element={<HomePage />} />
       <Route path="/reports" element={<Reports />} />
       <Route path="/aboutus" element={<AboutUs />} />
@@ -22,10 +44,8 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgotpassword" element={<ForgotPassword />} />
-      <Route path="/sales" element={<SalesPage />} />
-      <Route path="/pricing" element={<PaymentPage />} />
     </Routes>
-    );
-};
+  );
+}
 
 export default App;
