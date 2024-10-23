d5f125f7c3ab6a096d9963f5f421bb23948fd1ee
terenceouma <doros.devs@gmail.com>
2024-10-23 14:36:58 +0300

    changes to endpoints

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hfkk8p239qg"
+    "revision": "0.7log5bjc8ao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
